146514809753b90c312cff133e3a05a61f225b77
Adrián Rodríguez Armenta <adrianrodriguezarmenta@MacBook-Pro-de-Adrian.local>
2020-03-10 11:06:27 -0600

    tercer commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sql = require('mssql');
+// const sql = require('mssql');
 const app = express();
 // const routes = require('./routes'); 
 const port = process.env.PORT || 3000;
