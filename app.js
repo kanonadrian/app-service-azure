9c6e75d970e41303982d44e40fff34d7697056d2
Adrián Rodríguez Armenta <adrianrodriguezarmenta@MacBook-Pro-de-Adrian.local>
2020-03-10 10:50:15 -0600

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const sql = require('mssql');
 const app = express();
-const routes = require('./routes'); 
+// const routes = require('./routes'); 
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
-app.use('/service', routes);
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json()); 
+// app.use('/service', routes);
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
